5a39f5a63481e6253c51831e222624903bb5c823
Abdulla Zulqarnain <zul@Abdullas-MacBook-Pro.local>
2023-12-05 19:14:13 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [DatabaseModule, MoviesModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/movies/movies.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database/database.module';
-
-@Module({
-    imports: [DatabaseModule],
-})
-export class MoviesModule {}
